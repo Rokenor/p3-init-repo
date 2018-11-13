2cd4dc354a874a80e36ed7ddd57ecee6dd77f1b5
Oleksandr Kovrizhnykh <rokenor@zeoalliance.com>
2018-11-13 15:46:09 +0200

    Add additional assets

--- src/index.js
+++ src/index.js
@@ -2,25 +2,35 @@ import Phaser from 'phaser'
 import BootScene from './scenes/BootScene'
 import MenuScene from './scenes/MenuScene'
 import GameScene from './scenes/GameScene'
+import ArcadeScene from './scenes/ArcadeScene'
+
+// var config = {
+//     type: Phaser.AUTO,
+//     width:1024,
+//     height: 768,
+//     backgroundColor: 0xffdeb9,
+//     physics: {
+//         default: 'arcade',
+//         arcade: {
+//             gravity: {
+//                 y : 300 
+//             },
+//             debug: false
+//         }
+//     },
+//     scene: [
+//         BootScene,
+//         MenuScene,
+//         GameScene
+//     ]
+// }
 
 var config = {
     type: Phaser.AUTO,
-    width:1024,
-    height: 768,
-    backgroundColor: 0xffdeb9,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y : 300 
-            },
-            debug: false
-        }
-    },
+    width: 800,
+    height: 600,
     scene: [
-        BootScene,
-        MenuScene,
-        GameScene
+        ArcadeScene
     ]
 }
 
---
+++ src/scenes/ArcadeScene.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Scene {
+    constructor() {
+        super({ key: 'ArcadeScene' })
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+        
+    }
+}
